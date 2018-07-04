a50dc3babc24748125f98edcda55243e0942dd9a
stybik <bikrocksbiswas@gmail.com>
2018-07-05 00:45:32 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,21 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New User Connected');
 
+  //socket.emit from admin with text 'Welcome to chat app'
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+  //socket.broadcast.emit from admin text new user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User joined',
+    createdAt: new Date().getTime()
+  });
+  
+  
+  
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -21,6 +36,13 @@ io.on('connection', (socket)=>{
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    //broadcasting to everyone
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', ()=>{

149fe60cebd1763d4d3db76dabb1e344cb959d51
stybik <bikrocksbiswas@gmail.com>
2018-07-05 00:31:05 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New User Connected');
 
-  socket.emit('newMessage', {
-    from: 'bikram@gmail.com',
-    text: 'Hello world',
-    createdAt: 123123
-  })
-
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', ()=>{

7b6b65b40510058eb92f337e996506abe156bd83
stybik <bikrocksbiswas@gmail.com>
2018-07-05 02:01:51 +0530

    added form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
+const {generateMessage} = require('./utils/message');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
@@ -22,9 +22,10 @@ io.on('connection', (socket)=>{
   
   
   
-  socket.on('createMessage', (message)=>{
+  socket.on('createMessage', (message, callback)=>{
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
 
     //broadcasting to everyone
     // socket.broadcast.emit('newMessage', {
